5a4ef7312d4f07e3aeb68ce9d6ce47324abe74d8
wilgnerschuertz <60265271+wilgnerschuertz@users.noreply.github.com>
2022-01-11 15:26:32 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+  debugShowCheckedModeBanner: false,
+));
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  
+  List _lista = ['Teste1','Teste2','Teste3'];
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('W I D G E T S'),),
+        body: Column(
+          children: [
+            Expanded(child: ListView.builder(
+                itemCount: _lista.length,
+                itemBuilder: (context, index){
+
+                  final item = _lista[index];
+
+            return Dismissible(
+                background: Container(
+                  color: Colors.blue,
+                  padding: EdgeInsets.all(16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.edit,
+                        color: Colors.white,
+                      )
+                    ],),
+                ),
+                secondaryBackground: Container(
+                  color: Colors.red,
+                  padding: EdgeInsets.all(16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                    Icon(
+                      Icons.clear,
+                       color: Colors.white,
+                    )
+                  ],),
+                ),
+                // direction: DismissDirection.horizontal,
+                onDismissed: (direction){
+                  if( direction == DismissDirection.endToStart){
+                    print('Direita Esquerda');
+                  }else if( direction == DismissDirection.startToEnd){
+                    print('Esquerda Direita');
+                  }
+                },
+                key: Key(item),
+                child: ListTile(
+                  title: Text(item),
+                ));
+                }))
+          ],
+        ),
+    );
+  }
+}
